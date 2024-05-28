3b3dd0018ab29aa3dc8bf915a28555ed2f2a70e4
borghiste <158812327+borghiste@users.noreply.github.com>
2024-05-28 15:01:29 +0200

    styling

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let hamburger= document.querySelector('#hamburger');
+let sideBar = document.querySelector('#side-bar');
 
 hamburger.addEventListener('click', ()=>{
     hamburger.classList.toggle('closed');
+    didebar.classList.toggle('active');
    
 })
